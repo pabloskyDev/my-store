50d411b1ce903e6c79c42e21ed8453594ed64af5
Juan Pablo Betancur M <jpbetancur1999@gmail.com>
2022-10-14 09:50:02 -0500

    String Interpolation

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,7 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'my-store';
+  name = 'Pablo';
+  age = 18;
+  img = 'https://nightsky.jpl.nasa.gov/images/news/HDP_896.jpg';
 }
