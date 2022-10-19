e2d79166fb2144f517da3e869bba22de1561d946
Juan Pablo Betancur M <jpbetancur1999@gmail.com>
2022-10-18 20:00:01 -0500

    learning how works @input with components and the comunication between father and son

---
+++ src/app/components/img/img.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-img',
+  templateUrl: './img.component.html',
+  styleUrls: ['./img.component.scss']
+})
+export class ImgComponent implements OnInit {
+
+  @Input() img: string = 'Img init';
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
