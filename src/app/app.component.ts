cdeb081f15fe51c9ad94299f867f3d2085f7ce66
Juan Pablo Betancur M <jpbetancur1999@gmail.com>
2022-11-02 22:46:28 -0500

    Download files with Http

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FilesService } from './services/files.service';
 
 @Component({
   selector: 'app-root',
@@ -9,11 +10,20 @@ export class AppComponent {
   imgParent = '';
   showImg = true;
 
+  constructor(
+    private filesService: FilesService
+  ){}
+
   onLoaded(event: string) {
     console.log('Log padre ', event);
   }
 
   toggleImg() {
     this.showImg = !this.showImg;
+  }
+
+  downloadPdf() {
+    this.filesService.getFile('my.pdf', 'https://young-sands-07814.herokuapp.com/api/files/dummy.pdf', 'application/pdf')
+    .subscribe()
   }
 }
