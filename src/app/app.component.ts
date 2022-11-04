56cb4630bbd7e3ba0e90a374e29cd5a2a97c88cc
Juan Pablo Betancur M <jpbetancur1999@gmail.com>
2022-11-03 22:29:34 -0500

    Create a function to let upload files with angular

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,6 +9,7 @@ import { FilesService } from './services/files.service';
 export class AppComponent {
   imgParent = '';
   showImg = true;
+  imgRta = '';
 
   constructor(
     private filesService: FilesService
@@ -25,5 +26,16 @@ export class AppComponent {
   downloadPdf() {
     this.filesService.getFile('my.pdf', 'https://young-sands-07814.herokuapp.com/api/files/dummy.pdf', 'application/pdf')
     .subscribe()
+  }
+
+  onUpload(event: Event) {
+    const element = event.target as HTMLInputElement;
+    const file = element.files?.item(0) as Blob;
+    if(file) {
+      this.filesService.uploadFile(file)
+      .subscribe(rta => {
+        this.imgRta = rta.location
+      })
+    }
   }
 }
