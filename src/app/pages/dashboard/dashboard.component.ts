13a2f1bb6d6c2bb2360b2a76d37d2bf712342349
Juan Pablo Betancur M <jpbetancur1999@gmail.com>
2023-01-31 23:41:49 -0500

    [ADD] Adding search and links in product-list

--- src/app/pages/dashboard/dashboard.component.ts
+++ src/app/pages/dashboard/dashboard.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder } from '@angular/forms';
 import { FilesService } from 'src/app/services/files.service';
 
 @Component({
@@ -8,37 +9,51 @@ import { FilesService } from 'src/app/services/files.service';
 })
 export class DashboardComponent implements OnInit {
 
+  searchProducts = this.fb.nonNullable.group({
+    search: ['']
+  });
+
   imgRta = '';
   imgParent = '';
   showImg = true;
+
   constructor(
-    private filesService: FilesService
+    private filesService: FilesService,
+    private fb: FormBuilder
   ) { }
 
   ngOnInit(): void {
   }
 
+  onSubmit() {
+    const valSearch = this.searchProducts.getRawValue().search;
+    // TODO create a filterPipe and set limit and offset
+    if(valSearch) {
+      console.log(valSearch);
+    }
+  }
+
   // Todo change the location of this codes
   onLoaded(event: string) {}
 
-  toggleImg() {
-    this.showImg = !this.showImg;
-  }
+  // toggleImg() {
+  //   this.showImg = !this.showImg;
+  // }
 
-  downloadPdf() {
-    this.filesService.getFile('my.pdf', 'https://young-sands-07814.herokuapp.com/api/files/dummy.pdf', 'application/pdf')
-    .subscribe()
-  }
+  // downloadPdf() {
+  //   this.filesService.getFile('my.pdf', 'https://young-sands-07814.herokuapp.com/api/files/dummy.pdf', 'application/pdf')
+  //   .subscribe()
+  // }
 
-  onUpload(event: Event) {
-    const element = event.target as HTMLInputElement;
-    const file = element.files?.item(0) as Blob;
-    if(file) {
-      this.filesService.uploadFile(file)
-      .subscribe(rta => {
-        this.imgRta = rta.location
-      })
-    }
-  }
+  // onUpload(event: Event) {
+  //   const element = event.target as HTMLInputElement;
+  //   const file = element.files?.item(0) as Blob;
+  //   if(file) {
+  //     this.filesService.uploadFile(file)
+  //     .subscribe(rta => {
+  //       this.imgRta = rta.location
+  //     })
+  //   }
+  // }
 
 }
