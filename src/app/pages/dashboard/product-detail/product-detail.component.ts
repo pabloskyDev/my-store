a7db390bb4a118e3768dffd23e5e341c2f372a11
Juan Pablo Betancur M <jpbetancur1999@gmail.com>
2023-02-07 20:34:32 -0500

    [ADD] Add product detail component initials

---
+++ src/app/pages/dashboard/product-detail/product-detail.component.ts
@@ -0,0 +1,18 @@
+import { Component, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-product-detail',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './product-detail.component.html',
+  styleUrls: ['./product-detail.component.scss']
+})
+export class ProductDetailComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
