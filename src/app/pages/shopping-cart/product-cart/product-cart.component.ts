14251ce0509de1380e97112d0af8cac02b94aa4d
Juan Pablo Betancur M <jpbetancur1999@gmail.com>
2023-02-22 23:00:00 -0500

    [ADD] Add component of products of cart

---
+++ src/app/pages/shopping-cart/product-cart/product-cart.component.ts
@@ -0,0 +1,21 @@
+import { Component, OnInit, Input } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Product } from 'src/app/models/product.model';
+
+@Component({
+  selector: 'app-product-cart',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './product-cart.component.html',
+  styleUrls: ['./product-cart.component.scss']
+})
+export class ProductCartComponent implements OnInit {
+
+  @Input() product!: Product;
+  constructor() { }
+
+  ngOnInit(): void {
+    console.log(this.product);
+  }
+
+}
